57fd804acf9631a4a142440b4e80005ef9b22f35
Open Fibers <openfibers@gmail.com>
2013-08-15 17:36:35 +0800

    todo changed

--- OTNotification/Private/Windows/OTNotificationWindow.h
+++ OTNotification/Private/Windows/OTNotificationWindow.h
@@ -7,7 +7,6 @@
 //  A window simulate push notifications in iOS6.
 //  TODO:
 //      touch in cube, delegate
-//      shadow of notificaton window
 //      Add notification manager
 
 #import "ComOpenthreadOTNotificationRotateWindow.h"
