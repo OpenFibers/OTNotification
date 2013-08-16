1896bf2db2b3ce4ab050f889c0429da351083cfd
Open Fibers <openfibers@gmail.com>
2013-08-16 14:00:53 +0800

    added custom notification view

---
+++ OTNotificationDemo/CustomNotificationView.h
@@ -0,0 +1,15 @@
+//
+//  CustomNotificationView.h
+//  OTNotificationDemo
+//
+//  Created by openthread on 8/16/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OTNotification.h"
+
+//Must conform to OTNotificationViewProtocol to handle 
+@interface CustomNotificationView : UIView <OTNotificationViewProtocol>
+
+@end
---
+++ OTNotificationDemo/CustomNotificationView.m
@@ -0,0 +1,31 @@
+//
+//  CustomNotificationView.m
+//  OTNotificationDemo
+//
+//  Created by openthread on 8/16/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import "CustomNotificationView.h"
+
+@implementation CustomNotificationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
